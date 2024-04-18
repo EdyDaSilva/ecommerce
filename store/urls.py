cb70e795c2376ed9c14ba021e595f27396b75e66
EdyDaSilva <ednedson@hotmail.com>
2024-04-18 14:20:30 +0100

    list product by category

--- store/urls.py
+++ store/urls.py
@@ -5,9 +5,14 @@ from . import views
 
 urlpatterns = [
 
+    # store main page
     path('', views.store, name='store'),
 
-    path('product/<slug:slug>/', views.product_info, name='product-info'),    
+    # single product
+    path('product/<slug:product_slug>/', views.product_info, name='product-info'),
+
+    # by category
+    path('search/<slug:category_slug>/', views.list_category, name='list-category'), 
 
 ]
 
