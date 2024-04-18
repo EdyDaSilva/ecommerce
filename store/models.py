fbeb4cc4265cac90979f6412cc16fa85a428134a
EdyDaSilva <ednedson@hotmail.com>
2024-04-18 13:22:19 +0100

    dynamic link for single page

--- store/models.py
+++ store/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.urls import reverse
+
 
 # Create your models here.
 
@@ -17,6 +19,11 @@ class Category(models.Model):
     def __str__(self):
 
         return self.name
+    
+
+    #def get_absolute_url(self):
+
+        #return reverse('list-category', args=[self.slug])
     
 
 
@@ -45,5 +52,10 @@ class Product(models.Model):
     def __str__(self):
 
         return self.title
+    
+
+    def get_absolute_url(self):
+
+        return reverse('product-info', args=[self.slug])
     
 
