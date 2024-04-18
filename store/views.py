04926f7b1877ebd001c3449e194840fec4d19c4f
EdyDaSilva <ednedson@hotmail.com>
2024-04-18 13:08:48 +0100

    basic product single page

--- store/views.py
+++ store/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-
 from . models import Category, Product
+from django.shortcuts import get_object_or_404
 
 
 def store(request):
@@ -19,4 +19,11 @@ def categories(request):
     return {'all_categories': all_categories}
 
 
+def product_info(request, slug):
+
+    product = get_object_or_404(Product, slug=slug)
+
+    context = {'product': product}
+
+    return render(request, 'store/product-info.html', context)
 
