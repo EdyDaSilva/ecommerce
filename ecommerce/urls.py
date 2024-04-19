b974fc74e49b468a007ef4e27a3f9a878e437dbd
EdyDaSilva <ednedson@hotmail.com>
2024-04-19 10:24:13 +0100

    base for shopping cart created

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -7,9 +7,14 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-               
+    # admin url           
     path('admin/', admin.site.urls),
+
+    # store app
     path('', include('store.urls')),
+
+    # cart app
+    path('cart/', include('cart.urls')),
  
 ]
 
