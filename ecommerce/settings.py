7fccb8c7cb674c7d7804c880603fe574460688ec
EdyDaSilva <ednedson@hotmail.com>
2024-04-20 14:02:34 +0100

    email host added to settings

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -147,5 +147,12 @@ MEDIA_ROOT = BASE_DIR / 'static/media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# email configuration setting
+#EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = '587'
+EMAIL_USE_TLS = 'True'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST_USER = 'coffedirectshop@gmail.com'
+EMAIL_HOST_PASSWORD = 'ytmocjicxguhubya'
