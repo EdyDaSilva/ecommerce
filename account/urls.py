11e86cc92664b3fac877b7a02b2a47e668715e78
EdyDaSilva <ednedson@hotmail.com>
2024-04-21 00:17:22 +0100

    password reset templates

--- account/urls.py
+++ account/urls.py
@@ -2,6 +2,8 @@
 from django.urls import path
 
 from . import views
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
 
@@ -28,6 +30,16 @@ urlpatterns = [
 
     path('delete-account', views.delete_account, name='delete-account'),
 
+
+    # password management
+
+    path('reset_password', auth_views.PasswordResetView.as_view(template_name="account/password/password-reset.html"), name='reset_password'),
+
+    path('reset_password_sent', auth_views.PasswordResetDoneView.as_view(template_name="account/password/password-reset-sent.html"), name='password_reset_done'),
+
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="account/password/password-reset-form.html"), name='password_reset_confirm'),
+
+    path('reset_password_complete', auth_views.PasswordResetCompleteView.as_view(template_name="account/password/password-reset-complete.html"), name='password_reset_complete')
 
 
 ]
