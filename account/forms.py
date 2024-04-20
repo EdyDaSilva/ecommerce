d2c9688ddca7f13b12744a42fe184e8acb6fb4e7
EdyDaSilva <ednedson@hotmail.com>
2024-04-20 23:05:40 +0100

    user validation to prevent use email already existing

--- account/forms.py
+++ account/forms.py
@@ -1,8 +1,13 @@
+
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
+
 from django import forms
+
 from django.forms.widgets import PasswordInput, TextInput
 
+
+# Registration form
 
 class CreateUserForm(UserCreationForm):
 
@@ -11,48 +16,89 @@ class CreateUserForm(UserCreationForm):
         model = User
         fields = ['username', 'email', 'password1', 'password2']
 
-
-    # email validation
+    
     def __init__(self, *args, **kwargs):
         super(CreateUserForm, self).__init__(*args, **kwargs)
 
-        # required field
-        self.fields['email'].required = True
-    
+        # Mark email as required
 
+        self.fields['email'].required = True
+
+
+    # Email validation
+    
     def clean_email(self):
+
         email = self.cleaned_data.get("email")
 
         if User.objects.filter(email=email).exists():
-            raise forms.ValidationError("This email is alreday in use")
 
-        if len(email) >= 150:
+            raise forms.ValidationError('This email is invalid')
+
+        # len function updated ###
+
+        if len(email) >= 350:
+
             raise forms.ValidationError("Your email is too long")
-        
-        return email
-    
 
-# login form
+
+        return email
+
+
+
+# Login form
+
 class LoginForm(AuthenticationForm):
 
     username = forms.CharField(widget=TextInput())
     password = forms.CharField(widget=PasswordInput())
 
 
-# update form
+
+# Update form
+
 class UpdateUserForm(forms.ModelForm):
+
     password = None
+
+
+    class Meta:
+
+        model = User
+
+        fields = ['username', 'email']
+        exclude = ['password1', 'password1']
+    
 
     def __init__(self, *args, **kwargs):
         super(UpdateUserForm, self).__init__(*args, **kwargs)
 
-        # required field
+        # Mark email as required
+
         self.fields['email'].required = True
 
-    class Meta:
-        model = User
+        
+    # Email validation
+    
+    def clean_email(self):
 
-        fields = ['username', 'email']
-        exclude = ['password1', 'password2']
+        email = self.cleaned_data.get("email")
+
+        if User.objects.filter(email=email).exclude(pk=self.instance.pk).exists():
+
+            raise forms.ValidationError('This email is invalid')
+
+        # len function updated ###
+
+        if len(email) >= 350:
+
+            raise forms.ValidationError("Your email is too long")
+
+
+        return email
+        
+        
+ 
+ 
 
     
