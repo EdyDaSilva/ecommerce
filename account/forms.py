8f21542054363e0a2d1345eaf415247bc1ce24f7
EdyDaSilva <ednedson@hotmail.com>
2024-04-20 11:11:17 +0100

    user registration form first style

--- account/forms.py
+++ account/forms.py
@@ -15,12 +15,17 @@ class CreateUserForm(UserCreationForm):
     def __init__(self, *args, **kwargs):
         super(CreateUserForm, self).__init__(*args, **kwargs)
 
+        # required field
+        self.fields['email'].required = True
+    
 
     def clean_email(self):
         email = self.cleaned_data.get("email")
 
         if User.objects.filter(email=email).exists():
-            raise forms.ValidationError("This email is not valid")
+            raise forms.ValidationError("This email is alreday in use")
 
-        if len(email >= 150):
+        if len(email) >= 150:
             raise forms.ValidationError("Your email is too long")
+        
+        return email
