e9df910756c05cd9ac86ad5d20a1b9b8e43b20bc
EdyDaSilva <ednedson@hotmail.com>
2024-04-21 21:39:14 +0100

    shipping template improvement

--- account/views.py
+++ account/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 # from django.template import ContextPopException
 from .forms import CreateUserForm, LoginForm, UpdateUserForm
+from payment.forms import ShippingForm
+from payment.models import ShippingAddress
 from django.contrib.auth.models import User
 from django.contrib.sites.shortcuts import get_current_site
 from . token import user_tokenizer_generate
@@ -8,7 +10,7 @@ from django.template.loader import render_to_string
 from django.utils.encoding import force_bytes, force_str
 from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 from django.contrib.auth.models import auth
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 
@@ -164,3 +166,51 @@ def delete_account(request):
         return redirect('store')
 
     return render(request, 'account/delete-account.html')
+
+
+# shipping view
+@login_required(login_url='my-login')
+def manage_shipping(request):
+
+    try:
+
+        # Account user with shipment information
+
+        shipping = ShippingAddress.objects.get(user=request.user.id)
+
+
+    except ShippingAddress.DoesNotExist:
+
+        # Account user with no shipment information
+
+        shipping = None
+
+    form = ShippingForm(instance=shipping)
+
+    if request.method == 'POST':
+
+        form = ShippingForm(request.POST, instance=shipping)
+
+        if form.is_valid():
+
+            # Assign the user FK on the object
+
+            shipping_user = form.save(commit=False)
+
+            # Adding the FK itself
+
+            shipping_user.user = request.user
+
+
+            shipping_user.save()
+
+            # messages.info(request, "Update success!")
+
+            return redirect('dashboard')
+
+
+    context = {'form':form}
+
+    return render(request, 'account/manage-shipping.html', context=context)
+ 
+ 
