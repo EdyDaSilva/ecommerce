d1bb50334fbb6ba8054858917f0280cd2d60ef0d
EdyDaSilva <ednedson@hotmail.com>
2024-04-21 23:07:02 +0100

    cart functionality

--- payment/views.py
+++ payment/views.py
@@ -1,9 +1,36 @@
 from django.shortcuts import render
+from . models import ShippingAddress
+#from cart.cart import Cart
 
 # Create your views here.
 
 def checkout(request):
-    return render(request, 'payment/checkout.html')
+
+    # Users with accounts -- Pre-fill the form
+
+    if request.user.is_authenticated:
+
+        try:
+
+            # Authenticated users WITH shipping information 
+
+            shipping_address = ShippingAddress.objects.get(user=request.user.id)
+
+            context = {'shipping': shipping_address}
+
+            return render(request, 'payment/checkout.html', context=context)
+
+        except:
+
+            # Authenticated users with NO shipping information
+
+            return render(request, 'payment/checkout.html')
+
+    else:
+            
+        # Guest users
+
+        return render(request, 'payment/checkout.html')
 
 
 def payment_success(request):
