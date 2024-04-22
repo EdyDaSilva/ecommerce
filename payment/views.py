7016ac6ff5e2e7ee9385fb0e45f01f90fb12465d
EdyDaSilva <ednedson@hotmail.com>
2024-04-22 10:12:54 +0100

    redirection and clearing shopping cart

--- payment/views.py
+++ payment/views.py
@@ -126,6 +126,15 @@ def complete_order(request):
 
 
 def payment_success(request):
+
+    # Clear shopping cart
+    for key in list(request.session.keys()):
+
+        if key == 'session_key':
+
+            del request.session[key]
+
+            
     return render(request, 'payment/payment-success.html')
 
 
